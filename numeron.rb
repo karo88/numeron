dc906108e293a25b0b989e38cf931da5beda20f8
Daichi ONODERA <onodes@onod.es>
2013-02-27 18:01:25 +0900

    Add validation in TODO

--- numeron.rb
+++ numeron.rb
@@ -7,8 +7,15 @@ player = Numeron::Player.new(game, [1, 2, 3])
 
 loop do
   puts "Call your numbers:"
-  call_str = gets.chomp
-  # TODO validate call_str: length, is number?, number duplication
+  while call_str = gets.chomp
+    # TODO validate call_str: length, is number?, number duplication
+    if /^\d{#{game.secret_length}}$/ =~ call_str
+      break
+    else
+      puts "[Error] please input number, 3 digit"
+      puts "Call your numbers:"
+    end
+  end
   numbers = call_str.chars.map(&:to_i)
   eat, bite = player.judge(numbers)
   puts "%s: %d eat, %d bite" % [numbers.join, eat, bite]
